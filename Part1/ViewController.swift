248ad3bd9f4626533c88f7e752c15ea286ad337d
Nonoka Yamamoto <garden.n1234567890@gmail.com>
2020-07-15 14:04:55 +0900

    足し算機能追加

--- Part1/ViewController.swift
+++ Part1/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var textField1: UITextField!
+    @IBOutlet var textField2: UITextField!
+    @IBOutlet var textField3: UITextField!
+    @IBOutlet var textField4: UITextField!
+    @IBOutlet var textField5: UITextField!
+    @IBOutlet var answerLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField1.becomeFirstResponder()
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
     }
 
-
+    
+    @IBAction func add(_ sender: Any) {
+        
+        
+        let answer: Int = passNum(textField1) + passNum(textField2) + passNum(textField3) + passNum(textField4) + passNum(textField5)
+        answerLabel.text = String(answer)
+    }
+    
+    func passNum(_ textField: UITextField) -> Int {
+        if textField.text != nil {
+            if let stringNum = textField.text {
+                return Int(stringNum) ?? 0
+            } else {
+                return 0
+            }
+        } else {
+            return 0
+        }
+    }
 }
 
